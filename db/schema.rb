750a068fc85307051383a9c93f886ba6ca2e5471
callumhutchy <callumhutchie@hotmail.co.uk>
2017-12-09 16:50:22 +0000

    Maybe this works

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171207230801) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "broadcasts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
